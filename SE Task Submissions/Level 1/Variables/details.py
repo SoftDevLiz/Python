0fb8928c2eacad2112b713f1b2b09a558a5e48c7
Liezl Mouton <liz.mouton1@gmail.com>
2026-05-29 20:13:43 +0200

    Add missing tasks

---
+++ SE Task Submissions/Level 1/Variables/details.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+
+print(f"{name} is {age} and lives at {house_number} {street_name}.")
---
+++ SE Task Submissions/Level 1/Your First Computer Program/hello_world.py
@@ -0,0 +1,10 @@
+# use input() to get user inputs
+# use print() to output to terminal
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+print("Hello, World!")
